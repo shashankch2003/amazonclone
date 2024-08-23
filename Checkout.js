3879cd6e02c9aa7fbe237f7aff99c6badaed73a1
shashankch2003 <122424766+shashankch2003@users.noreply.github.com>
2024-08-23 07:46:36 +0530

    Update Checkout.js

--- Checkout.js
+++ Checkout.js
@@ -9,8 +9,7 @@ function Checkout() {
       <div className="Checkout">
           <div className='checkout__left'>
               <img className='checkout__ad'  src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" 
-                  alt=""
-              />
+                  alt=""/>
               <div>
                   <h2 className='checkout__title'>
                       your shopping basket to you
