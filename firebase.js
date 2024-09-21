4351dd9c960af00366caf1bb055ee2cac4a8cc41
shashankch2003 <122424766+shashankch2003@users.noreply.github.com>
2024-09-21 22:31:02 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -11,7 +11,9 @@ const firebaseConfig = {
     appId: "1:1018779785923:web:c58f2f7773db20ca9ea076",
     measurementId: "G-MRN05HK07Y"
 };
-  
+
+
+
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebaseApp.auth();
