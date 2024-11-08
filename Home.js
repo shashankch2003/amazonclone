16dd1a2e547270613e42eda0557ce392650a97eb
shashankch2003 <122424766+shashankch2003@users.noreply.github.com>
2024-11-08 21:57:30 +0530

    Update Home.js

--- Home.js
+++ Home.js
@@ -11,6 +11,7 @@ function Home() {
                     src='https://images-eu.ssl-images-amazon.com/images/G/31/img22/Wireless/devjyoti/GW/Uber/Nov/D103625178_DesktopTallHero_3000x1200._CB574597993_.jpg'
                     alt=''
                 />
+                        
                 <div className='home__row'>
                     <Product
                         title='Chandelier Ceiling Lamp 300mm Glass Crystal Big Size Jhumar Lamp fo Living Room/Hall'
