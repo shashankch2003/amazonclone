88538e9751ddad0a1a4e88b8a44dc79668b0abd5
shashankch2003 <122424766+shashankch2003@users.noreply.github.com>
2024-12-02 15:30:25 +0530

    Update Header.js

--- Header.js
+++ Header.js
@@ -50,7 +50,6 @@ function Header() {
                   <span className='header__optionLineTwoheader__basketCount'>{ basket?.length }</span>
               </div>
               </Link>
-            
          </div>
         
      </div>
